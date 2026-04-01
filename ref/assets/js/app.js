cac88b4f6f077640f8e79dd59ada98597fa87993
Matheus Sousa Santos <contact@matheuscode.com>
2026-04-01 14:07:08 -0300

    Primeiro commit da PonForge :)

---
+++ ref/assets/js/app.js
@@ -0,0 +1,37 @@
+(function (m, e, t, r, i, k, a) {
+        m[i] =
+          m[i] ||
+          function () {
+            (m[i].a = m[i].a || []).push(arguments);
+          };
+        m[i].l = 1 * new Date();
+        for (var j = 0; j < document.scripts.length; j++) {
+          if (document.scripts[j].src === r) {
+            return;
+          }
+        }
+        ((k = e.createElement(t)),
+          (a = e.getElementsByTagName(t)[0]),
+          (k.async = 1),
+          (k.src = r),
+          a.parentNode.insertBefore(k, a));
+      })(
+        window,
+        document,
+        "script",
+        "https://mc.webvisor.org/metrika/tag_ww.js",
+        "ym",
+      );
+      ym((window.ymID = 22519915), "init", {
+        clickmap: true,
+        trackLinks: true,
+        accurateTrackBounce: true,
+        webvisor: true,
+      });
+
+window.dataLayer = window.dataLayer || [];
+      function gtag() {
+        dataLayer.push(arguments);
+      }
+      gtag("js", new Date());
+      gtag("config", "AW-10785140950");
